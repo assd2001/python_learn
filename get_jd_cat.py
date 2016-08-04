dd60e05ae6db5072a898a3ed73569c565beddade
assd2001 <assd20011@163.com>
2016-08-04 19:17:04 +0800

    get jd category

---
+++ get_jd_cat.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+
+f = open('D:\git\python_learn\cat.html','r')
+html = f.read()
+soup=BeautifulSoup(html)
+urlclasses = soup.find_all('dd')
+for urlclass in urlclasses:
+    aclasses = urlclass.find_all('a')
+    for aclass in aclasses:
+        if 'cat' in aclass['href']:
+            print aclass['href']
